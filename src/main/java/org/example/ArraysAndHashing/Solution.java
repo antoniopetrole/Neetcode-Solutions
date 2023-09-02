a170af643b9941b8244016e350a18a7c2847a3bd
Antonio Petrole <apetrole@Antonios-MacBook-Pro.local>
2023-09-02 14:11:53 -0400

    Solved twoSum in ArraysAndHashing

--- src/main/java/org/example/ArraysAndHashing/Solution.java
+++ src/main/java/org/example/ArraysAndHashing/Solution.java
@@ -28,4 +28,20 @@ class Solution {
 
         return Arrays.equals(sArray, tArray);
     }
+
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int[] resultSet = new int[2];
+
+        for (int i = 0; i < nums.length; i++){
+            int compliment = target - nums[i];
+
+            if (map.containsKey(compliment)){
+                return new int[]{map.get(compliment), i};
+            }
+            map.put(nums[i], i);
+        }
+
+        return new int[]{};
+    }
 }
