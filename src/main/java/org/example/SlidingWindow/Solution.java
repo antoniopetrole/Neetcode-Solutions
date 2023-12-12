1d045da2028a679e386cbc2bd3f787e19e0a91a0
antoniopetrole <antoniopetrole@gmail.com>
2023-12-11 20:05:01 -0500

    Length of longest substring

--- src/main/java/org/example/SlidingWindow/Solution.java
+++ src/main/java/org/example/SlidingWindow/Solution.java
@@ -19,4 +19,30 @@ public class Solution {
 
         return bestPrice;
     }
+
+    public int lengthOfLongestSubstring(String s) {
+
+        if(s.length() < 2){
+            return s.length();
+        }
+
+        int l = 0;
+        int r = 1;
+        int length = 0;
+        String substring = s.charAt(l) + "";
+
+
+        while(r < s.length()){
+            if(!substring.contains(s.charAt(r) + "")){
+                substring += s.charAt(r);
+                r++;
+            } else {
+                length = Math.max(substring.length(), length);
+                l++;
+                r = l + 1;
+                substring = s.charAt(l) + "";
+            }
+        }
+        return length;
+    }
 }
--- src/test/java/org/example/SlidingWindow/SlidingWindow.java
+++ src/test/java/org/example/SlidingWindow/SlidingWindow.java
@@ -11,4 +11,24 @@ public class SlidingWindow {
 
         assertEquals(2, solution.maxProfit(testData));
     }
+
+    @Test
+    public void lengthOfLongestSubstring(){
+        String test1 = "abcabcbb";
+
+        assertEquals(3, solution.lengthOfLongestSubstring(test1));
+
+        String test2 = "bbbbb";
+
+        assertEquals(1, solution.lengthOfLongestSubstring(test2));
+
+        String test3 = "pwwkew";
+
+        assertEquals(3, solution.lengthOfLongestSubstring(test3));
+
+        String test4 = "au";
+
+        assertEquals(2, solution.lengthOfLongestSubstring(test4));
+
+    }
 }
