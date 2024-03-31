7c9aec3d83c6d44e186fa151e67a72b72580010a
antoniopetrole <antoniopetrole@gmail.com>
2024-03-31 12:43:31 -0400

    Finished rough implementation of linkedlist

--- src/main/java/org/example/DSA/LinkedList.java
+++ src/main/java/org/example/DSA/LinkedList.java
@@ -6,49 +6,66 @@ public class LinkedList {
     // LinkedList() will initialize an empty linked list.
     private Node head;
     private Node tail;
+    private int size = 0;
 
     public LinkedList() {
-        head = null;
-        tail = null;
+        head = new Node(-1);
+        tail = head;
     }
 
     // int get(int i) will return the value of the ith node (0-indexed). If the index is out of bounds, return -1.
     public int get(int index) {
-        return 0;
+        Node current = head.next;
+        int pointer = 0;
+        while (current != null){
+            if (pointer == index){
+                return current.value;
+            }
+            pointer++;
+            current = current.next;
+        }
+        return -1;
     }
 
     // void insertHead(int val) will insert a node with val at the head of the list.
     public void insertHead(int val) {
-        if (head == null){
-            head = new Node(val);
-            tail = head;
-        } else {
-            head = new Node(val, this.head);
+        Node newNode = new Node(val);
+        newNode.next = head.next;
+        head.next = newNode;
+        if (newNode.next == null){
+            tail = newNode;
         }
     }
 
     // void insertTail(int val) will insert a node with val at the tail of the list.
     public void insertTail(int val) {
-        if (tail == null){
-            tail = new Node(val);
-            head = tail;
-        } else {
-            Node temp = new Node(val);
-            this.tail.next = temp;
-            this.tail = temp;
-        }
+        this.tail.next = new Node(val);
+        this.tail = this.tail.next;
     }
 
     // int remove(int i) will remove the ith node (0-indexed). If the index is out of bounds, return false, otherwise return true.
     public boolean remove(int index) {
+        int pointer = 0;
+        Node current = head;
+        while(pointer < index && current != null){
+            current = current.next;
+            pointer++;
+        }
+
+        if (current != null && current.next != null){
+            if (current.next == this.tail) {
+                this.tail = current;
+            }
+            current.next = current.next.next;
+            return true;
+        }
         return false;
     }
 
     // int[] getValues() return an array of all the values in the linked list, ordered from head to tail.
     public ArrayList<Integer> getValues() {
         ArrayList<Integer> llAsArrayList = new ArrayList<>();
-        Node current = head;
-
+        Node current = head.next;
         while (current != null){
             llAsArrayList.add(current.value);
             current = current.next;
--- src/test/java/org/example/DSA/LinkedListTests.java
+++ src/test/java/org/example/DSA/LinkedListTests.java
@@ -13,7 +13,7 @@ public class LinkedListTests {
     public void insertHead(){
         LinkedList linkedList = new LinkedList();
 
-        assertEquals(linkedList.getValues(), new ArrayList<Integer>());
+        assertEquals(new ArrayList<Integer>(), linkedList.getValues());
 
         linkedList.insertHead(5);
         assertEquals(new ArrayList<Integer>(Arrays.asList(5)), linkedList.getValues());
@@ -35,4 +35,22 @@ public class LinkedListTests {
         assertEquals(new ArrayList<Integer>(Arrays.asList(5, 10)), linkedList.getValues());
     }
 
+    @Test
+    public void remove(){
+        LinkedList linkedList = new LinkedList();
+
+        linkedList.insertTail(5);
+        linkedList.insertTail(10);
+        linkedList.insertTail(43);
+        linkedList.remove(1);
+        assertEquals(new ArrayList<Integer>(Arrays.asList(5,43)), linkedList.getValues());
+    }
+
+    @Test
+    public void ncFailedCase(){
+        LinkedList linkedList = new LinkedList();
+        linkedList.insertHead(1);
+        Boolean result = linkedList.remove(0);
+        assertEquals(true, result);
+    }
 }
